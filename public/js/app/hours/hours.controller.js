e4a4279100022c6115f8c032da93cde5bad7a925
Jesu Marquez <jesumarquez@gmail.com>
2015-07-07 21:05:06 +0000

    hours controller list items

--- public/js/app/hours/hours.controller.js
+++ public/js/app/hours/hours.controller.js
@@ -2,5 +2,14 @@ angular.module('MolineteApp.Hours', [])
 .controller('HoursController', ['$scope', HoursController]);
 
 function HoursController($scope){
- $scope.name = 'demo';
+ $scope.hours = [
+     {
+         id : '1',
+         date : '07/07/2015 9:45'
+     },
+     {
+         id : '2',
+         date : '07/07/2015 18:10'
+     }
+ ];
 }
