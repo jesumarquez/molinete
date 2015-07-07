39f9da8a134361aa62482a02d4e5e0e3a14cd030
Jesu Marquez <jesumarquez@gmail.com>
2015-07-07 20:56:57 +0000

    add about and contact pages

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -5,6 +5,12 @@ angular.module('MolineteApp',['ngRoute','MolineteApp.Hours'])
           when('/', {
             templateUrl: 'partials/articles.html'
           }).
+          when('/about', {
+            templateUrl: 'partials/about.html'
+          }).
+          when('/contact', {
+            templateUrl: 'partials/contact.html'
+          }).
           when('/hours', {
               templateUrl: '/js/app/hours/hours.view.html',
               controller: 'HoursController'
