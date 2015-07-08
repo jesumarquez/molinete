fa2ec78c8479e5dd7e7c8c25899d4dd680a3b6ed
Jesu Marquez <jesumarquez@gmail.com>
2015-07-08 20:21:39 +0000

    Defining Hours data  presentation

--- public/js/app/hours/hours.controller.js
+++ public/js/app/hours/hours.controller.js
@@ -2,14 +2,76 @@ angular.module('MolineteApp.Hours', [])
 .controller('HoursController', ['$scope', HoursController]);
 
 function HoursController($scope){
- $scope.hours = [
-     {
-         id : '1',
-         date : '07/07/2015 9:45'
-     },
-     {
-         id : '2',
-         date : '07/07/2015 18:10'
-     }
+ $scope.dates = [
+   {
+      id : 1,
+      day : {
+         dayName : 'Yesterday',
+         hours : 8,
+         minutes : 15,
+         ranges : [
+            {
+               hours : 4,
+               minutes : 0,
+               events : [
+                  {
+                     time : '09:15',
+                     eventType : 'Enter'
+                  },
+                  {
+                     time : '12:35',
+                     eventType : 'Leave'
+                  }]
+            },
+            {
+               hours : 4,
+               minutes : 0,
+               events : [
+                  {
+                     time : '12:55',
+                     eventType : 'Enter'
+                  },
+                  {
+                     time : '18:45',
+                     eventType : 'Leave'
+                  }]
+            }]
+      }
+   },
+   {
+      id : 2,
+      day : {
+         dayName : 'Tuesday',
+         hours : 8,
+         minutes : 15,
+         ranges : [
+            {
+               hours : 4,
+               minutes : 0,
+               events : [
+                  {
+                     time : '09:15',
+                     eventType : 'Enter'
+                  },
+                  {
+                     time : '12:35',
+                     eventType : 'Leave'
+                  }]
+            },
+            {
+               hours : 4,
+               minutes : 0,
+               events : [
+                  {
+                     time : '12:55',
+                     eventType : 'Enter'
+                  },
+                  {
+                     time : '18:45',
+                     eventType : 'Leave'
+                  }]
+            }]
+      }
+   }   
  ];
 }
